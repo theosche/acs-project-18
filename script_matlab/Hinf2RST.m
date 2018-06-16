e383efde64a5a61825eb3cbe8d11dc1556d82152
hugo Grall Lucas <hugo.gralllucas@epfl.ch>
2018-06-16 23:11:04 +0200

    update4

---
+++ script_matlab/Hinf2RST.m
@@ -0,0 +1,16 @@
+temp = cell2mat(K3.Z);
+num = [1];
+for j= 1:length(temp)
+    num = conv([1,-temp(j)],num);
+end
+num = num * K3.K;
+temp = cell2mat(K3.P);
+den = [1];
+for j= 1:length(temp)
+    den = conv([1,-temp(j)],den);
+end
+
+R1 = num;
+T1 = num;
+S1 = den;
+save('RSTG1','R1','S1','T1');
